fd8dd625a44ce6d90230ce08fb38787f3413a553
Pedro Augusto <peedroaugustto@hotmail.com>
2020-02-27 15:24:16 -0300

    Correção do primeiro commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'everis-header',
+  templateUrl: './header.component.html',
+
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
